d25a46a3da7862e8b453cebaa455fe6d329d2ea4
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2024-12-03 17:26:04 -0600

    Fix logic for part 2

--- day03/index.ts
+++ day03/index.ts
@@ -18,45 +18,26 @@ console.log("==============");
 console.log(total);
 // ----------------------------------------------
 
-// TODO: FINISH
-type MatchRange = {
-    start: number,
-    end: number
-}
-
-const conditionalRegex = /do(n't)?\(\)/g;
+const conditionalRegex = /do(?:n't)?\(\)|mul\(([0-9]+),([0-9]+)\)/g;
 const condMatches = [...file.matchAll(conditionalRegex)]
 
-const doMatch = condMatches.filter((e) => e[0] === "do()");
-const dontMatch = condMatches.filter((e) => e[0] === "don't()");
-
-for (const currentMatch of dontMatch) {
-    let nextDo: number = -1;
-
-    for (const dos of doMatch) {
-        if (dos.index > currentMatch.index) {
-            nextDo = dos.index + dos[0].length;
-            break;
+let enabled = true;
+total = 0;
+for (const match of condMatches) {
+    if (match[0] === "do()") {
+        enabled = true;
+    } else if (match[0] === "don't()") {
+        enabled = false;
+    } else {
+        if (enabled) {
+            total += (Number(match[1]) * Number(match[2]))
         }
-        nextDo = -1;
     }
-    console.log(currentMatch.index+ "->" +nextDo);
-    
-    matches = matches.filter((obj) => {
-        if (nextDo === -1) return !(obj.index >= currentMatch.index);
-        return (obj.index < currentMatch.index) || (obj.index > nextDo)
-    })
 }
-matches.forEach((mt) => {
-    console.log(mt.index)
-})
+
+
 
 console.log("==============");
 console.log("Part 2");
 console.log("==============");
-
-total = 0;
-for (const mul of matches) {
-    total += Number(mul[1]) * Number(mul[2]);
-}
 console.log(total);
